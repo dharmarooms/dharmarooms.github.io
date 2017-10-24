123569c44e3421223a6f41ad6cabd788e105f5a7
playra <raoffonom@icloud.com>
2017-10-24 17:12:03 +0300

    done donate

--- web/src/App.js
+++ web/src/App.js
@@ -1,57 +1,129 @@
 import React, { Component } from 'react'
 import logo from './logo.png'
 import './App.css'
+import { Textfit } from 'react-textfit'
+import ReactFontFace from 'react-font-face'
 
 const styles = {
+  header: {
+    fontFamily: 'Alegreya Sans SC',
+    color: '#DBD7D2',
+    alignItems: 'center', 
+    justifyContent: 'center',
+  },
   mission: {
     fontFamily: 'AppleSDGothicNeo-Light',
-    fontSize: 15,
     lineHeight: '14px',
     color: '#DBD7D2',
-    alignItems: 'center', justifyContent: 'center'
+    alignItems: 'center', 
+    justifyContent: 'center',
+    paddingTop: 5
   },
   total: {
     fontFamily: 'AppleSDGothicNeo-Light',
-    fontSize: 16,
     lineHeight: '1px',
     color: 'white',
-    alignItems: 'center', justifyContent: 'center'
+    alignItems: 'center', 
+    justifyContent: 'center',
+    paddingTop: 15
   },
   namaste: {
-    fontFamily: 'AppleSDGothicNeo-Light',
-    fontSize: 16,
-    lineHeight: '3px',
-    color: '#DBD7D2',
-    alignItems: 'center', justifyContent: 'center'
-  }
+    fontfamily: 'applesdgothicneo-light',
+    lineheight: '3px',
+    color: '#dbd7d2',
+    alignitems: 'center', 
+    justifycontent: 'center',
+    paddingTop: 10
+  },
+  benefit: {
+    fontfamily: 'applesdgothicneo-light',
+    lineheight: '3px',
+    color: '#474747',
+    alignitems: 'center', 
+    justifycontent: 'center',
+    paddingTop: 15
+  },
+  names: {
+    fontfamily: 'applesdgothicneo-light',
+    lineheight: '3px',
+    color: '#474747',
+    alignitems: 'center', 
+    justifycontent: 'center',
+    paddingTop: 15
+  },
 }
 
 const donate = 49
 
 class App extends Component {
   render() {
-    const { namaste, mission, total } = styles
+
+    const { header, namaste, mission, total, benefit, names } = styles
+
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">#Дхарма</h1>
-          <p style={mission}> 
-            Миссия приложения - это проведение света дхармы в мир адхармы и Ваша поддержка позволит уделять нашему общему делу больше времени. <br/>В планах пополнить библиотеку более 700 духовными книгами и перевести на все языки мира. 
+          <Textfit 
+            min={2}
+            max={30}
+            style={header} 
+            mode="single"
+          >
+            #Дхарма 
+          </Textfit>
+          <Textfit 
+            style={ mission } 
+            mode="multi"
+          >
+            Миссия приложения - это проведение света дхармы в мир адхармы и Ваша поддержка позволит уделять нашему общему делу больше времени. <br/>В планах пополнить библиотеку более 700 духовными книгами и перевести на основные языки мира. 
             <br/>Благодаря выбранному цифровому формату эти книги будут существовать не одно поколение духовных искателей.
-          </p>
-          <p style={total}> 
+          </Textfit>
+          <Textfit 
+            min={3}
+            max={16}
+            style={total} 
+            mode="single"
+          >
             Всего собрано средств: {donate} р.
-          </p>
-          <p style={namaste}> 
+          </Textfit>
+          <Textfit 
+            min={8}
+            max={15}
+            style={namaste}
+            mode="single"
+          >
             Намасте 
-          </p>
-          
+          </Textfit>
         </header>
-        <iframe title="Донейшн" src="https://money.yandex.ru/quickpay/shop-widget?writer=seller&targets=%D0%94%D0%BE%D0%BD%D0%B5%D0%B9%D1%88%D0%BD&targets-hint=&default-sum=&button-text=11&payment-type-choice=on&hint=&successURL=&quickpay=shop&account=410015631248809" width="350" height="198" frameborder="0" allowtransparency="true" scrolling="no"></iframe>
+        <iframe title='Донейшн' src="https://money.yandex.ru/quickpay/shop-widget?writer=seller&targets=%D0%94%D0%BE%D0%BD%D0%B5%D0%B9%D1%88%D0%BD&targets-hint=&default-sum=&button-text=11&payment-type-choice=on&fio=on&hint=&successURL=&quickpay=shop&account=410015631248809" width="325" height="198" frameborder="0" allowtransparency="true" scrolling="no"></iframe>
+        <Textfit 
+          min={8}
+          max={20}
+          style={benefit}
+          mode="single"
+        >
+          Благотворители:
+        </Textfit>
+        <Textfit 
+          min={8}
+          max={38}
+          style={names} 
+          mode="multi"
+        >
+          Гея Камская
+        </Textfit>
       </div>
     )
   }
 }
 
-export default App
+let fontConfig = {
+  google: [
+    'Pangolin',
+    'Alegreya Sans SC',
+    'Roboto Mono',
+  ]
+}
+
+export default ReactFontFace(App, fontConfig)
